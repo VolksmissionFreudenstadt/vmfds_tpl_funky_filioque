2d45a3a893020d17cfd95375b346bbfd4cec25e4
Christoph Fischer <chris@toph.de>
2015-05-13 15:32:26 +0200

    Labels in ext_tables.php angepasst

--- ext_tables.php
+++ ext_tables.php
@@ -3,7 +3,7 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Site templates');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Vorlagen und Einstellungen, Funky Filioque');
 \FluidTYPO3\Flux\Core::registerProviderExtensionKey('vmfds', 'Page');
 \FluidTYPO3\Flux\Core::registerProviderExtensionKey('vmfds', 'Content');
 
